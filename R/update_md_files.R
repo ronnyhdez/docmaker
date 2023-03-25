7ec6090c697dde96f4ea0724d152b41fdeea6f62
ronnyhdez <ronny.hernandezm@gmail.com>
2023-03-24 21:32:00 -0600

    Ref #34 function ready with conditionals for several cases

--- R/update_md_files.R
+++ R/update_md_files.R
@@ -1,9 +1,22 @@
-# Update md files
-
 library(dplyr)
-library(tidyr)
-library(fs)
 
+#' @title Update your md files
+#' 
+#' @author Ronny Alexander Hernández Mora
+#' 
+#' @description This function will take your notes written in markdown directly
+#' to your docs folder. If a note is updated, use this function to validate it
+#' and update the docs/ directory.
+#' 
+#' @param path The path where your directory with your notes are located. If
+#' empty, it will take the path of the repository you are located.
+#' 
+#' @examples 
+#' \dontrun{
+#' update_md_files("~/Desktop/test_docmaker")
+#'}
+#'
+#' @export
 update_md_files <- function(path = "here") {
   
   # Create objects with paths
@@ -22,13 +35,13 @@ update_md_files <- function(path = "here") {
   
   # Identify md files in the root directory
   # path <- "~/Documents/repos/github/drawer" 
-  path <- "~/Desktop/test_docmaker"
-  files_root <- dir_info(path) %>% 
+  # path <- "~/Desktop/test_docmaker"
+  files_root <- fs::dir_info(path) %>% 
     filter(type == "file",
            grepl("\\.md$", path)) %>% 
     select(path, modification_time, change_time, birth_time) %>% 
     mutate(path = as.character(path)) %>% 
-    separate(col = path, into = c("path", "file"), sep = ".*/") %>% 
+    tidyr::separate(col = path, into = c("path", "file"), sep = ".*/") %>% 
     select(-path) %>% 
     filter(file != "README.md") %>% 
     rename("mod_time_root" = "modification_time",
@@ -37,10 +50,10 @@ update_md_files <- function(path = "here") {
   
   # Identify md files in the `docs` directory
   docs_path <- paste0(path, "/docs")
-  files_docs <- dir_info(docs_path) %>% 
+  files_docs <- fs::dir_info(docs_path) %>% 
     filter(type == "file") %>% 
     select(path, modification_time, change_time, birth_time) %>% 
-    separate(col = path, into = c("path", "file"), sep = ".*/") %>% 
+    tidyr::separate(col = path, into = c("path", "file"), sep = ".*/") %>% 
     select(-path) 
   
   # If none of the files in root are in docs, copy them
@@ -49,27 +62,32 @@ update_md_files <- function(path = "here") {
       select(file) %>% 
       pull() %>% 
       purrr::map(~ file_copy(file.path(path, .x), 
-                             file.path(docs_path, .x)))
+                             file.path(docs_path, .x),
+                             overwrite = TRUE))
     
-    return("Files are updated!")
+    print("Files are updated!")
     
   } else {
     # Join dataframes with the `files_root` as reference
     both <- files_root %>% 
       left_join(files_docs, by = join_by(file)) %>% 
       # If a file exists in root but not in docs, treat the NA
-      mutate(change_time = tidyr::replace_na(change_time, lubridate::make_datetime())) %>%
+      mutate(change_time = tidyr::replace_na(change_time, 
+                                             lubridate::make_datetime())) %>%
       # Check which files in root are more updated than the ones in `docs`
       filter(change_time_root > change_time) %>% 
       select(file) %>% 
       pull()
     
     if (length(both) == 0) {
-      return("No files in docs dicrectory are outdated")
+      print("No files in docs dicrectory are outdated")
     } else {
       both %>% 
         purrr::map(~ file_copy(file.path(path, .x), 
-                               file.path(docs_path, .x)))
+                               file.path(docs_path, .x),
+                               overwrite = TRUE))
+      
+      print("Files are updated!")
     }
   }
 }

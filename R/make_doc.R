0c16e271d11f89905b17ff6209be0aa3cd5c1cdd
ronnyhdez <ronny.hernandezm@gmail.com>
2023-04-11 15:37:07 -0600

    Ref #36 function tested to include qmd documents

--- R/make_doc.R
+++ R/make_doc.R
@@ -29,24 +29,24 @@ make_doc <- function(file, mkdocs_build = FALSE, mkdocs_deploy = FALSE) {
   file_extension
   # Remove path and keep just the file name and extension
   original_file_name <- stringr::str_remove(file, ".*/")
-   original_file_name  
+  original_file_name  
   
   # Change qmd or Rmd file extension to md.
   file_name <- paste0(fs::path_ext_remove(original_file_name), ".md")
-   file_name  
+  file_name  
   # Render the file in here
-  if (file_extension == "Rmd") {
-  rmarkdown::render(input = file, 
-                    output_file = file_name,
-                    output_format = "md_document")
-  } else if (file_extension == "qmd") {
-	  quarto::quarto_render(input = file,
-							output_file = file_name,
-							output_format = "md_document")
+  if (file_extension == ".Rmd") {
+    rmarkdown::render(input = file, 
+                      output_file = file_name,
+                      output_format = "md_document")
+  } else if (file_extension == ".qmd") {
+    quarto::quarto_render(input = file,
+                          output_file = file_name,
+                          output_format = "md")
   } else {
-	  stop("File format not supported")
+    stop("File format not supported")
   }
-
+  
   # Copy new md file to docs/ folder (where site in rendered)
   fs::file_move(file_name, new_path = "docs/")
   

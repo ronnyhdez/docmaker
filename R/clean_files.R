6b8ef55c1378a4d228762bc8a46eff3560badb88
ronnyhdez <ronny.hernandezm@gmail.com>
2023-04-11 19:10:02 -0600

    Ref #44 function updated to return clear messages of the process

--- R/clean_files.R
+++ R/clean_files.R
@@ -1,10 +1,28 @@
+#' @import dplyr
 
-
-clean_files <- function(path) {
+#' @title Clean files in the docs folder that no longer exists in the root path.
+#' 
+#' @author Ronny Alexander Hernández Mora
+#' 
+#' @description If you have erased no longer needed files in the root directory,
+#' the render `md` files will continue to exists in the docs folder and hence, 
+#' they will continue to appear in your webpage. This function checks if there
+#' are erased files in the root and delete them from the docs folder.
+#' 
+#' @param path The path where your directory with your notes are located. If
+#' empty, it will take the path of the repository you are located.
+#' 
+#' @examples 
+#' \dontrun{
+#' clean_files("~/Desktop/test_docmaker")
+#'}
+#'
+#' @export
+clean_files <- function(path = "here") {
   # Create objects with paths
   if (path == "here") {
     root_path <- here::here()
-    docs_path <- paste0(path, "/docs")
+    docs_path <- paste0(root_path, "/docs")
     
   } else {
     root_path <- path
@@ -12,7 +30,7 @@ clean_files <- function(path) {
   }
   
   # Validate if paths exists
-  stopifnot("Path does not exists" = fs::dir_exists(path))
+  stopifnot("Path does not exists" = fs::dir_exists(root_path))
   stopifnot("There is no docs directory in path" = fs::dir_exists(docs_path))
   
   # docs_path <- "/home/ronny/Documents/repos/github/drawer/docs"
@@ -58,6 +76,10 @@ clean_files <- function(path) {
     })
     
     fs::file_delete(files_to_erase)
+    
+    message <- paste("These files have been erased from `docs/`:",
+                     paste(both, collapse = ", "))
+    print(message)
   }
 }
 
